b85c78ec8f88f67c4b7eb9c73a7a1bb02919dd92
monfadev <mfaauzii@gmail.com>
2022-06-02 18:31:03 +0700

    Migrating Android Embedding v2

--- charts_flutter/example/android/app/src/main/java/com/example/examples/MainActivity.java
+++ charts_flutter/example/android/app/src/main/java/com/example/examples/MainActivity.java
@@ -1,17 +1,6 @@
 package com.example.examples;
 
-import android.os.Bundle;
+import io.flutter.embedding.android.FlutterActivity;
 
-import io.flutter.app.FlutterActivity;
-import io.flutter.plugins.GeneratedPluginRegistrant;
-
-/**
- * FlutterActivity
- */
 public class MainActivity extends FlutterActivity {
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    GeneratedPluginRegistrant.registerWith(this);
-  }
 }
